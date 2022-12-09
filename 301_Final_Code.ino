2048ed4a295aff7d7b1f5142e92928ffb6967762
1001-Lee-Eric <77991645+1001-Lee-Eric@users.noreply.github.com>
2022-12-09 10:29:56 -0800

    Code Merger
    Merge all code into one file

---
+++ 301_Final_Code.ino
@@ -0,0 +1,21 @@
+arduino library 
+stepper motor
+lcd
+clock
+dht11 sensor
+#include <Stepper.h>
+#include <LiquidCrystal.h>
+#include <Wire.h>
+#include <DS3231.h>
+#include <dht_nonblocking.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
